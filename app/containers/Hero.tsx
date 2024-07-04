ad3d93b995cae1cd3aa98f3a3c01ef9f16d6bb5d
Noureddine Sidi <noureddinesidiabed6k@gmail.com>
2024-07-04 02:11:51 -0400

    added video to hero section

--- app/containers/Hero.tsx
+++ app/containers/Hero.tsx
@@ -3,7 +3,17 @@ import Section from "../components/Section";
 const Hero = () => {
   return (
     <Section>
-      <div>Hero</div>
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      >
+        <source src="/videos/boxerbag.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="absolute top-0 left-0  bg-black opacity-70 z-0">Hero</div>
     </Section>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import About from "./containers/About";
 import Schedule from "./containers/Schedule";
 import Gallery from "./containers/Gallery";
 import Training from "./containers/Training";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
