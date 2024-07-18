c425cd1fbb07452077494cc4a01fe25d136425ed
Noureddine Sidi Abed <noureddinesidiabed6k@gmail.com>
2024-07-17 22:19:53 -0400

    Changed hero section styles

--- app/containers/Hero.tsx
+++ app/containers/Hero.tsx
@@ -13,12 +13,27 @@ const Hero = () => {
           <source src="/videos/boxerbag.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 saturate-200 flex flex-col items-center justify-center">
-          <div className="text-white text-7xl font-bold">FITBOX</div>
-
-          <div className="text-center text-white mt-8 p-6 rounded-xl ">
-            <h1 className="text-2xl font-bold ">Monday - Friday</h1>
-            <p className="text-2xl ">9am - 6pm</p>
+        <div className=" absolute top-0  w-full h-full bg-black bg-opacity-70 saturate-200 flex flex-col items-start justify-center pl-12">
+          <div className="px-3">
+            <div className="text-white text-7xl font-bold mt-4 leading-snug">
+              FitBox Brooklyn
+            </div>
+            <div className="text-white text-5xl font-normal mt-2">
+              Train With A Professional Boxer
+            </div>
+            <div className="text-white text-xl font mt-6 w-2/6 ">
+              At FitBox, we offer a comprehensive boxing training program
+              designed to help you master the best techniques while achieving
+              your unique fitness goals.
+            </div>
+            <div className="mt-14 flex space-x-4  h-12 w-2/6">
+              <button className="w-44 border border-black bg-white text-black px-6 py-3 rounded-md font-semibold">
+                Make the change
+              </button>
+              <button className="w-44 border border-black bg-gray-800 text-white px-6 py-3 rounded-md font-semibold">
+                Book Session
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- app/containers/Trainers.tsx
+++ app/containers/Trainers.tsx
@@ -3,10 +3,10 @@ import { Section } from "../components/Section";
 const Trainers = () => {
   return (
     <Section id="trainer">
-      <div className="h-full w-full p-32 bg-black flex items-center justify-center">
+      <div className="h-full w-full px-40 py-32 bg-black flex items-center justify-center">
         <div className="flex-shrink-0 bg-white w-1/3 h-full">d</div>
         <div className="flex flex-col lg:flex-row items-center justify-center w-full ">
-          <div className="text-white my-8 mx-5 lg:mt-0 lg:ml-16">
+          <div className="text-white px-20 my-8 mx-5 lg:mt-0 lg:ml-16">
             <h2 className="text-4xl font-bold mb-4 w-1/2">Meet The Coach</h2>
             <p className="text-lg leading-relaxed mb-8">
               Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum
