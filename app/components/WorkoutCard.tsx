a311348e93f11f4fb196b80894d4627052a8d594
Carlos Flores <carlosfloresalex@gmail.com>
2024-08-11 00:05:41 -0400

    added mobile responsive behavior

--- app/components/WorkoutCard.tsx
+++ app/components/WorkoutCard.tsx
@@ -20,7 +20,7 @@ const WorkoutCard: React.FC<WorkoutCardProps> = ({
   const handleMouseEnter = () => {
     setIsHovered(true);
     if (videoRef.current) {
-      videoRef.current.currentTime = 2;
+      videoRef.current.currentTime = 0;
       videoRef.current.play();
     }
   };
@@ -35,7 +35,7 @@ const WorkoutCard: React.FC<WorkoutCardProps> = ({
 
   return (
     <div
-      className="relative rounded-lg shadow-lg overflow-hidden bg-black m-4 max-w-sm flex-shrink-0 h-5/6"
+      className="2xl:max-h-none max-h-96 relative rounded-lg shadow-lg overflow-hidden bg-black flex-1"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
@@ -71,16 +71,16 @@ const WorkoutCard: React.FC<WorkoutCardProps> = ({
           )
         )}
       </div>
-      <h2 className="text-xl font-bold absolute top-4 left-4 text-white w-full p-2">
-        {headerText}
-      </h2>
 
       <div
-        className={`p-4 absolute bottom-0 left-0 w-full h-1/4 opacity-75 bg-black text-white transition-transform duration-500 ${
-          isHovered ? "transform translate-y-0" : "transform translate-y-full"
+        className={`p-6 absolute bottom-16 left-0 w-full h-60 opacity-75 bg-black text-white transition-transform duration-500 ${
+          isHovered ? "transform translate-y-20" : "transform translate-y-full"
         }`}
       >
-        {otherText && <p className="text-gray-300 mt-2">{otherText}</p>}
+        <h2 className="text-xl font-bold text-white w-full mb-2">
+          {headerText}
+        </h2>
+        <p className="text-gray-300">{otherText}</p>
       </div>
     </div>
   );
--- app/containers/Programs.tsx
+++ app/containers/Programs.tsx
@@ -11,34 +11,9 @@ const Programs = () => {
         {/* Background Blur Effect */}
         <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-lg z-0 m-0 p-0"></div>
 
-        {/* Mobile layout */}
-        <div className="relative block sm:hidden z-10 h-full">
-          <div className="flex flex-wrap justify-center gap-4 px-4 py-0 items-center m-0 p-0 h-full">
-            <WorkoutCardHorizontal
-              headerText="FITNESS BOXING"
-              videoLink="/videos/fitboxvideo.mp4"
-              imageLink="/images/fitbox.png"
-              otherText="Improve natural power, get stronger, build stamina, maximize your muscle endurance and resistance. 
-              ALL LEVELS. Meet your goals to get leaner and gain muscle by following a personalized and effective program."
-            />
-            <WorkoutCardHorizontal
-              headerText="BOXING LESSONS: BEGINNER TO ADVANCED"
-              videoLink="/videos/resting.mp4"
-              imageLink="/images/fitbox.png"
-              otherText=" Learn the fundamentals to increase boxing knowledge and improve technical boxing skills."
-            />
-            <WorkoutCardHorizontal
-              headerText="STRENGTH AND CONDITIONING"
-              videoLink="/videos/WeightTraining.mp4"
-              imageLink="/images/weight.png"
-              otherText="Build more muscles."
-            />
-          </div>
-        </div>
-
         {/* Desktop layout */}
-        <div className="relative hidden sm:block z-10 h-full">
-          <div className="h-4/5 flex flex-wrap justify-center gap-4 md:gap-20 px-4 py-0 items-center m-0 p-0 ">
+        <div className="flex h-full">
+          <div className="container flex-col 2xl:flex-row m-auto flex gap-10">
             <WorkoutCard
               headerText="FITNESS BOXING"
               videoLink="/videos/fitboxvideo.mp4"
