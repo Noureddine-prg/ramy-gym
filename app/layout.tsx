0760c4827b7ff45d7622282a9b673d94e3a56e64
Noureddine Sidi <noureddinesidiabed6k@gmail.com>
2024-09-22 01:55:42 -0400

    Make sure to npm i. Added lenis and implemented smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,36 @@
+"use client";
+
 import Navbar from "./components/Navbar";
 import "./styles/Global.css";
 import Footer from "./components/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
-    <html lang="en" className="overflow-x-hidden !scroll-smooth bg-[#181818]">
-      <body className="flex flex-col">
+    <html lang="en" className="overflow-x-hidden">
+      <body className="flex flex-col bg-black">
         <Navbar />
         {children}
         <Footer />
